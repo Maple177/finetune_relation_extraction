224d7f3d30aa3d01a3371d0b090c276cdf8f2a46
FrancisTang <hence.tangdonald@outlook.com>
2023-03-21 22:46:18 +0100

    Update opt.py

--- opt.py
+++ opt.py
@@ -42,7 +42,7 @@ def get_args():
 		         help="Denote this value by p; p * number_of_total_steps will be used for warmup i.e. "
 		              "at the end of p * number_of_total_steps, learning rate will attain the target value.")
      group.add_argument("--logging_steps", type=int, default=50,
-                         help="Denote this value by X; Log the training loss every X updates steps.")
+                         help="Denote this value by X; Log the training loss every X steps.")
      group.add_argument("--do_not_save_all_models",action="store_true",
                          help="Set this to save disk space: if not set, we save ensemble_size checkpoints for all runs "
                               "(which may require too much disk space).")
